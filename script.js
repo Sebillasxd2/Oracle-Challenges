d0f8f693791384143ab6f2786f4604cf9161e975
Sebillasxd2 <jsebastianarduzespa@gmail.com>
2024-03-04 21:00:04 -0400

    desafio_1 G6

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
 const texto = document.querySelector("#texto1");
 const resultado = document.querySelector("#texto2");
 
-    function btnEncriptar(){
-        const textoEncriptado = encriptar(texto.value);
-        resultado.value = textoEncriptado;
-        texto.value="";
-    }
+function btnEncriptar(){
+    const textoEncriptado = encriptar(texto.value);
+    resultado.value = textoEncriptado;
+    texto.value="";
+}
 
-    function encriptar(mensaje){
-        let matrizEncriptado = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
-        mensaje = mensaje.toLowerCase();
-
-        for(let i = 0; i < matrizEncriptado.length; i++){
-            if(mensaje.includes(matrizEncriptado[i][0])){
-                mensaje = mensaje.replaceAll(matrizEncriptado[i][0],matrizEncriptado[i][1]);   
-            }else{
-                alert("no introdujiste algo valido para encriptar");
-                break;
-
-            }
+function encriptar(mensaje){
+    let matrizEncriptado = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    mensaje = mensaje.toLowerCase();
+    verificar = mensaje.toLowerCase();
+    for(let i = 0; i < matrizEncriptado.length; i++){
+        if(mensaje.includes(matrizEncriptado[i][0])){
+            mensaje = mensaje.replaceAll(matrizEncriptado[i][0],matrizEncriptado[i][1]);   
         }
+    }
+    if(verificar === mensaje){
+        alert("El texto no puede ser encriptado, no hay nada que desencriptar");
+        mensaje = "";
         return mensaje;
-    }
-    function btnDesencriptar(){
-        const textoEncriptado = desencriptar(texto.value);
-        resultado.value = textoEncriptado;
-        texto.value="";
-    }
+    }else{
+    return mensaje;}
+}
 
-    function desencriptar(mensaje){
-        let matrizEncriptado = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
-        mensaje = mensaje.toLowerCase();
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(texto.value);
+    resultado.value = textoEncriptado;
+    texto.value="";
+}
 
-        for(let i = 0; i < matrizEncriptado.length; i++){
-            if(mensaje.includes(matrizEncriptado[i][1])){
-                mensaje = mensaje.replaceAll(matrizEncriptado[i][1],matrizEncriptado[i][0]);   
-            }else{
-                alert("no introdujiste algo valido para desencriptar");
-                break;
-            }
+function desencriptar(mensaje){
+    let matrizEncriptado = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    mensaje = mensaje.toLowerCase();
+    verificar = mensaje.toLowerCase();
+
+    for(let i = 0; i < matrizEncriptado.length; i++){
+        if(mensaje.includes(matrizEncriptado[i][1])){
+            mensaje = mensaje.replaceAll(matrizEncriptado[i][1],matrizEncriptado[i][0]);   
         }
+    }
+    if(verificar === mensaje){
+        alert("El texto no puede ser desencriptado, no hay nada que desencriptar");
+        mensaje = "";
         return mensaje;
-    }
-    function copiar(){
-        const mensajecopiado = document.querySelector(".textoresuelto");
-        mensajecopiado.select();
-        document.execCommand('copy');
-    }
+    }else{
+    return mensaje;
+}}
+
+function copiar(){
+    const mensajecopiado = document.querySelector(".textoresuelto");
+    mensajecopiado.select();
+    document.execCommand('copy');
+}
